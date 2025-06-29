954b925220fedb877e73046d66682f6fca2e2ad6
mennatallahessam <mennatallah_essam@aucegypt.edu>
2025-06-29 13:47:34 +0300

    clone

---
+++ cmake-build-debug/CMakeFiles/assembly_project.dir/initial_code.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/assembly_project.dir/initial_code.cpp.obj: \
+ C:\Users\AUC\assembly_project\initial\ code.cpp
---
+++ cmake-build-debug/CMakeFiles/assembly_project.dir/zx_16_simulator.cpp.obj.d
@@ -0,0 +1,2 @@
+CMakeFiles/assembly_project.dir/zx_16_simulator.cpp.obj: \
+ C:\Users\AUC\assembly_project\zx_16_simulator.cpp
