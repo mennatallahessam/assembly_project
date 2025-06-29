11d8bb741458e9a28636363907c1e3c9be4aba05
mennatallahessam <mennatallah_essam@aucegypt.edu>
2025-06-29 13:13:59 +0300

    project working code for 2 ecalls

---
+++ cmake-build-debug/CMakeFiles/assembly_project.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/assembly_project.dir/initial_code.cpp.obj"
+  "CMakeFiles/assembly_project.dir/initial_code.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/alu.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/alu.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/decoder.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/decoder.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/ecalls.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/ecalls.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/main.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/main.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/memory.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/memory.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/registers.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/registers.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/simulator.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/simulator.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/src/utils.cpp.obj"
+  "CMakeFiles/assembly_project.dir/src/utils.cpp.obj.d"
+  "CMakeFiles/assembly_project.dir/zx_16_simulator.cpp.obj"
+  "CMakeFiles/assembly_project.dir/zx_16_simulator.cpp.obj.d"
+  "assembly_project.exe"
+  "assembly_project.exe.manifest"
+  "assembly_project.pdb"
+  "libassembly_project.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/assembly_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
