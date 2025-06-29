88ccba7972006027b98c6b124d7ccfe1937e973a
mennatallahessam <mennatallah_essam@aucegypt.edu>
2025-06-29 13:44:24 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 int main() {
-    std::string programPath = "C:/Users/ASUS/Desktop/ecall_test2.bin";  // Hardcoded file path
+    std::string programPath = "C:/Users/AUC/Downloads/ecall_test2.bin";  // Hardcoded file path
 
     Simulator sim;
     sim.run(programPath);
