0edda3b8e711d1e0d1d84967c87cb0930ab8a8a2
fishawiii <124688808+fishawiii@users.noreply.github.com>
2025-06-30 10:12:16 +0300

    Update test_add_print.asm

--- test_add_print.asm
+++ test_add_print.asm
@@ -1,21 +1,16 @@
 .text
 .org 0x000
     j main
-
 .org 0x0020
 main:
     # x1 = 12
     addi x1, 12
-
     # x2 = 8
     addi x2, 8
-
     # ---- Addition ----
     add x6, x1, x2    # x6 = x1 + x2 = 20
-
     addi r10, 10         # syscall: exit
     ecall
 
 
-    # Exit
     ecall 10
