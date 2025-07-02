8a9a7cbbcd881af768c5612f47557f4267911c1d
mennatallahessam <159177904+mennatallahessam@users.noreply.github.com>
2025-07-02 18:41:56 +0300

    Update alu.h

--- src/alu.h
+++ src/alu.h
@@ -1,21 +1,11 @@
 #pragma once
-#include "DecodedInstruction.h"
-#include "Registers.h"
-#include "Memory.h"
+#include "decoder.h"
+#include "registers.h"
+#include "memory.h"
+#include "ecalls.h"
+#include "graphics.h"
 
 class ALU {
 public:
-    void execute(const DecodedInstruction& instr, Registers& regs, Memory& mem, uint32_t& pc, bool& halted);
-
-private:
-    void executeRType(const DecodedInstruction& instr, Registers& regs);
-    void executeIType(const DecodedInstruction& instr, Registers& regs, uint32_t& pc);
-    void executeLoad(const DecodedInstruction& instr, Registers& regs, Memory& mem);
-    void executeStore(const DecodedInstruction& instr, Registers& regs, Memory& mem);
-    void executeBranch(const DecodedInstruction& instr, Registers& regs, uint32_t& pc);
-    void executeJAL(const DecodedInstruction& instr, Registers& regs, uint32_t& pc);
-    void executeJALR(const DecodedInstruction& instr, Registers& regs, uint32_t& pc);
-    void executeLUI(const DecodedInstruction& instr, Registers& regs);
-    void executeAUIPC(const DecodedInstruction& instr, Registers& regs, uint32_t pc);
-    void executeECALL(Registers& regs, Memory& mem, bool& halted);
+    void execute(const DecodedInstruction& instr, Registers& regs, Memory& mem, uint16_t& pc, bool& halted, Ecalls& ecalls, Graphics& gfx);
 };
