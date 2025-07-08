b96f283f4ad3e244d3836c475bfb3af2f84f371b
mennatallahessam <159177904+mennatallahessam@users.noreply.github.com>
2025-07-08 17:57:27 +0300

    Update graphics.h

--- src/graphics.h
+++ src/graphics.h
@@ -1,16 +1,22 @@
-//
-// Created by ASUS on 7/3/2025.
-//
-
+// graphics.h
 #ifndef GRAPHICS_H
 #define GRAPHICS_H
 
-
+#include <cstdint>
+#include <SDL2/SDL.h>
 
 class graphics {
+private:
+    SDL_AudioDeviceID audio_device;
+    uint8_t current_volume = SDL_MIX_MAXVOLUME; // Default to max volume
 
+public:
+    graphics();
+    ~graphics();
+    
+    void initAudio();
+    void setVolume(uint8_t volume);
+    uint8_t getVolume() const;
 };
 
-
-
-#endif //GRAPHICS_H
+#endif
